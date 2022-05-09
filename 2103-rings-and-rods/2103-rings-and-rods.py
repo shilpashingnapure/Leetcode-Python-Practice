57d678b70153e3c9adcdb198ebe5557f565119ff
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-05-09 11:19:57 +0530

    Time: 32 ms (85.43%), Space: 13.9 MB (65.36%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        d = {}
+        for i in range(0 , len(rings) , 2):
+            pos = rings[i+1]
+            ring = rings[i]
+            if(pos in d):
+                lst = d[pos]
+                if(ring not in lst):
+                    lst.append(ring)
+                
+            else:
+                d[pos] = [ring]
+        count = 0
+        for key , value in d.items():
+            if(len(value) == 3):
+                count += 1
+        return count
+            
+        
+        
+        
+            
+            
