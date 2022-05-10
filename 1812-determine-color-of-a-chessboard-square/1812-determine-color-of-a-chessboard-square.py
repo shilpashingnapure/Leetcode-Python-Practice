10c14f1880247b18b56fb79e94e7272d4c6fddb4
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-05-10 13:07:48 +0530

    Time: 51 ms (27.83%), Space: 13.9 MB (8.19%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -1,18 +1,14 @@
 class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
-        # matrix = [["white" for j in range(8)] for i in range(8)]
-        # for i in range(8):
-        #     for j in range(8):
-        #         if i % 2 == 0 and j%2 == 0:
-        #             matrix[i][j] = "black"
-        #         if i%2 == 1 and j % 2 == 1:
-        #             matrix[i][j] = "black"
+        matrix = [["white" for j in range(8)] for i in range(8)]
+        for i in range(8):
+            for j in range(8):
+                if i % 2 == 0 and j%2 == 0:
+                    matrix[i][j] = "black"
+                if i%2 == 1 and j % 2 == 1:
+                    matrix[i][j] = "black"
         d = "abcdefgh"
         x = d.index(coordinates[0])
         y = int(coordinates[1])-1
-        # return matrix[x][y] == "white"
-    
-        #using math method
-        if(x + y)%2 == 0:return False
-        return True
+        return matrix[x][y] == "white"
         
