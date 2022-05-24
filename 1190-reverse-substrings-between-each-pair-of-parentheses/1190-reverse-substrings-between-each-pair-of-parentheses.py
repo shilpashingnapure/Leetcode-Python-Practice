880e8ef8e00049788ce504296bf0e881fcee1215
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-05-24 10:58:45 +0530

    Time: 47 ms (41.86%), Space: 14 MB (21.99%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,19 +1,26 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
         s = list(s)
-        stack = []
+        stack = []   #<- storing the open parenthese indexs
         for i in range(len(s)):
             if s[i] == "(":
                 stack.append(i)
                 
             elif s[i] == ")":
                 
+                #index of open parentheses
                 start = stack.pop()
+                
+                #index of closing parenthese
                 end = i
+                
+                #reversing the substring between open and closing parenthese
                 rev = s[start+1:end]
                 
+                #updating that reverse substring
                 s[start+1:end] = rev[::-1]
                    
+        #removing parenthese in string            
         return ''.join(s).replace("(" , '').replace(")" , '')
                 
                  
