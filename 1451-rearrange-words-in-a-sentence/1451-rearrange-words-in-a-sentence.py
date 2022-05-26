21b0d46d27f149eb7af4f59bba609f09f1fb71f3
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-05-26 15:48:20 +0530

    Time: 103 ms (17.69%), Space: 18.1 MB (5.10%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -3,10 +3,9 @@ class Solution:
         arr = []
         text = text.split(" ")
         for i in text:
-            
             arr.append([i.lower() , len(i)])
         
         arr.sort(key=lambda  x : x[1])
         arr = [i[0] for i in arr]
         s = " ".join(arr)
-        return s[0].upper() + s[1:]
+        return s.capitalize()
