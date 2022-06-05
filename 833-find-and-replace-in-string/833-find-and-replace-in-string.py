2d4c7027a36e58cd0e08f351037147508578d56c
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-06-05 15:56:54 +0530

    Time: 54 ms (46.90%), Space: 14 MB (58.40%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -5,15 +5,24 @@ class Solution:
         j = 0
         while j < len(s):
             if j in indices:
+                
+                #if index is present then we getting index in indices list
                 index = indices.index(j)
+                
+                #finding substring in s 
                 print(s[j:j+len(sources[index])] , sources[index])
                 
+                #if substring is equal to sources and j equal to indices element add target value and increment by length of substring
                 if s[j:j+len(sources[index])] == sources[index] and j == indices[index]:
                     ans += targets[index]
                     j += len(sources[index])
+                    
+                #nothing to do
                 else:
                     ans += s[j]
                     j += 1
+                    
+            #nothing to do increment by 1
             else:
                 ans += s[j]
                 j += 1
