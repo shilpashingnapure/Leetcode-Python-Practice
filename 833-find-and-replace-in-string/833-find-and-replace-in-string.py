b713abd2bb21c94183d4c58ad73ed385e4dc44aa
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-06-05 15:50:34 +0530

    Time: 63 ms (28.77%), Space: 14 MB (20.40%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,21 +1,13 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        ans = ""
         
-        arr = []
-        for i in range(len(indices)):
-            a = indices[i]
-            b = sources[i]
-            c = targets[i]
-            arr.append([a , b , c])
-        arr.sort()
         ans = ""
         j = 0
         while j < len(s):
             if j in indices:
                 count = indices.index(j)
-                
                 print(s[j:j+len(sources[count])] , sources[count])
+                
                 if s[j:j+len(sources[count])] == sources[count] and j == indices[count]:
                     ans += targets[count]
                     j += len(sources[count])
