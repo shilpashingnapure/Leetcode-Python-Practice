5737cb84670d9bc66d011ffb3abd94ac29f2b598
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-07-20 19:14:16 +0530

    Time: 445 ms (29.29%), Space: 25.5 MB (27.59%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -2,60 +2,62 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        
+        Logic Behind -: 
+            e.g. 
+            
+            [1 2 3 4 5 6 7] , k = 3
+            1. reverse the 0 to k range [4,3,2,1]
+            2. reverse the k to N range [7,6,5]
+            it become like [4,3,2,1,7,6,5]
+            3. final reverse the whole array
+                [5,6,7,1,2,3,4]
         """
+        
+        #checking if k is greathen len 
         if k > len(nums):
             k = k % len(nums)
+            
+        #l to r is for 0 to k     
         l = 0
         r = len(nums)-k-1 
         
+        #l1 to r1 is for k to N
         l1 = len(nums) - k  
         r1 = len(nums) - 1  
         
-        print(l , r)
-        print(l1 , r1)
-        
+        #reversing both 0 to k and k to N
         while l <= r and l1 <= r1:
             nums[l] , nums[r] = nums[r] , nums[l]
             nums[l1] , nums[r1] = nums[r1] , nums[l1]
-            
-           
             l += 1
             r -= 1
             l1 += 1
             r1 -= 1
         
+        #checking if 0 to k range still not statifiy condition 
         while l <= r:
             nums[l] , nums[r] = nums[r] , nums[l]
             l += 1
             r -= 1
         
+        #checking if k to N range still not statifiy condition
         while l1 < r1:
             nums[l1] , nums[r1] = nums[r1] , nums[l1]
             l1 += 1
             r1 -= 1
         
+        #after done reversing whole array 
         left = 0
         right = len(nums) - 1
         while left <= right:
             nums[left] , nums[right] = nums[right] , nums[left]
             left += 1
             right -= 1
-        return nums
+        
     
         
-#         0 4
-#         1 2 3 4 5
-#         5 4 3 2 1
-#         6 7
-#         7 6
-        
-#         5 4 3 2 1 7 6 
-        # 6 7 1 2 3 4 5
-    
-    
-        
-      # 1  2 3 4 5 6
-      # 1 6 5 4 3 2
+
         
         
             
