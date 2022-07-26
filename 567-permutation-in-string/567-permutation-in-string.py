495fa8cbea82f949164bfa1896c5d9c5c10113e3
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-07-26 16:26:42 +0530

    Time: 1695 ms (22.90%), Space: 14.1 MB (31.36%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,24 +1,38 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):return False
         d = Counter(s1)
-        
-        left = 0
-        right = len(s1)
-        while right <= len(s2):
-            if self.match(left , right , s2 , d):
+        for i in range(len(s2)):
+            if s2[i] in d:
+                d[s2[i]] -= 1
+            if i >= len(s1) and s2[i-len(s1)] in d:
+                d[s2[i-len(s1)]] += 1
+            print(d)
+            if all([d[i] == 0 for i in d]):
                 return True
-            else:
-                left += 1
-                right += 1
         return False
-    def match(self , left , right , s2 , d):
-        d1 = {}
-        for i in range(left , right):
-            if s2[i] in d1:
-                d1[s2[i]] += 1
-            else:
-                d1[s2[i]] = 1
+#         if len(s1) > len(s2):return False
+#         d = Counter(s1)
         
-        return d == d1
+#         left = 0
+#         right = len(s1)
+#         while right <= len(s2):
+#             if self.match(left , right , s2 , d):
+#                 return True
+#             else:
+#                 left += 1
+#                 right += 1
+#         return False
+
+        
+#     def match(self , left , right , s2 , d):
+#         d1 = {}
+#         for i in range(left , right):
+#             if s2[i] in d1:
+#                 d1[s2[i]] += 1
+#             else:
+#                 d1[s2[i]] = 1
+        
+#         return d == d1
+            
     
+            
