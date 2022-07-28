df8e05dde3ddbb8267bc97564d0e8857c384c702
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-07-28 15:33:38 +0530

    Time: 92 ms (33.79%), Space: 13.9 MB (47.08%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,58 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        d = {1:0}
+        N = len(grid)
+        M = len(grid[0])
+        que = []
+        for i in range(N):
+            for j in range(M):
+                if grid[i][j] == 1:
+                    d[1] += 1
+                if grid[i][j] == 2:
+                    que.append((i , j))
+        if d[1] == 0:return 0
+        
+        def bfs(r , c):
+            count = 0
+            if r >= 1 and grid[r-1][c] == 1:
+                grid[r-1][c] = 2
+                que.append((r-1 , c))
+                count += 1
+            if r+1 < N and grid[r+1][c] == 1:
+                grid[r+1][c] = 2
+                que.append((r+1 , c))
+                count += 1
+                
+            if c >= 1 and grid[r][c-1] == 1:
+                grid[r][c-1] = 2
+                que.append((r , c-1))
+                count += 1
+                
+            if c+1 < M and grid[r][c+1] == 1:
+                grid[r][c+1] = 2
+                que.append((r , c+1))
+                count += 1
+            
+            return count
+        ans = 0
+        while que and d[1] > 0:
+            
+            ans += 1
+            for i in range(len(que)):
+                r,c = que.pop(0)
+                m = bfs(r,c)
+                d[1] -= m
+            
+            
+            
+        if d[1] != 0: return -1             
+        return ans
+ 
+    
+    
+    
+    
+    
+    
+    
+   
