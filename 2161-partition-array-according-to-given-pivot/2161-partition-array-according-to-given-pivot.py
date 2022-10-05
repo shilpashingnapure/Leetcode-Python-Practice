e2c121be2422fffd9376a2962d413834cdcc06f0
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-10-05 16:02:40 +0530

    Time: 3777 ms (12.48%), Space: 31.6 MB (57.09%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,13 +1,37 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        before = []
-        eq = []
-        after = []
+        #find out index of begin with high , low , same
+        
+        arr = [0] * len(nums)
+        low = eq = 0
         for i in nums:
             if i < pivot:
-                before.append(i)
+                low+=1
             elif i == pivot:
-                eq.append(i)
+                eq += 1
+        high = low + eq
+        eq = low
+        low = 0
+        for i in nums:
+            if i < pivot:
+                arr[low] = i
+                low+=1
+            elif i == pivot:
+                arr[eq] = i
+                eq += 1
             else:
-                after.append(i)
-        return before + eq + after
+                arr[high] = i
+                high += 1
+        return arr
+                
+        # before = []
+        # eq = []
+        # after = []
+        # for i in nums:
+        #     if i < pivot:
+        #         before.append(i)
+        #     elif i == pivot:
+        #         eq.append(i)
+        #     else:
+        #         after.append(i)
+        # return before + eq + after
