55552110a7352deef7909b6ee7428a3fc6af97be
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-10-10 11:57:43 +0530

    Time: 3859 ms (8.10%), Space: 27.5 MB (60.72%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,25 +1,41 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         i = j = 0
-        d = Counter()
+        arr = set()
         s = 0
-        ans = 0
+        m = 0
         while j < len(nums):
-            s += nums[j]
-            d[nums[j]] += 1
+            if nums[j] not in arr:
+                s += nums[j]
+                m = max(m , s)
+                arr.add(nums[j])
+                j += 1
+            else:
+                s -= nums[i]
+                arr.remove(nums[i])
+                i += 1
+        return m
+    
+    
+#         i = j = 0
+#         d = Counter()
+#         s = 0
+#         ans = 0
+#         while j < len(nums):
+#             s += nums[j]
+#             d[nums[j]] += 1
             
-            if len(d) == j - i + 1:  
-                ans = max(ans,s)
-                # print(s , d , j , i)
-            elif len(d) != j-i+1:
-                while len(d) != j - i + 1:
-                    d[nums[i]] -= 1
-                    if d[nums[i]] == 0:
-                        del d[nums[i]]
+#             if len(d) == j - i + 1:  
+#                 ans = max(ans,s)
+#             elif len(d) != j-i+1:
+#                 while len(d) != j - i + 1:
+#                     d[nums[i]] -= 1
+#                     if d[nums[i]] == 0:
+#                         del d[nums[i]]
                    
-                    s -= nums[i]
-                    i += 1
+#                     s -= nums[i]
+#                     i += 1
             
-            j += 1
-        return ans
+#             j += 1
+#         return ans
      
