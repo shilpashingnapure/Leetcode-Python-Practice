0f7c819bc2fcfff121d0b1307133eb999e622962
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-12-29 12:39:29 +0530

    Added solution - LeetHub

---
+++ Pattern 1 - GFG/pattern-1.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def printSquare(self, N):
+        # Code here
+        for i in range(N):
+            print(N * '* ')
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printSquare(N)
+# } Driver Code Ends
